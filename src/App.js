347e9c09d72c526ab31b8c2df546810bb98c0cbd
SamStratton2112 <109560024+SamStratton2112@users.noreply.github.com>
2024-10-09 19:37:40 -0700

    Work on Logo File

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import AllRoutes from './components/AllRoutes';
+import NavigationBar from './components/NavigationBar';
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
+      <NavigationBar/>
       <AllRoutes/>
     </BrowserRouter>
   );
