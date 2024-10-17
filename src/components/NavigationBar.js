abc037a7e5d529a0437015ec94018dfb850503db
SamStratton2112 <109560024+SamStratton2112@users.noreply.github.com>
2024-10-16 19:37:32 -0700

    clean up

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -13,7 +13,6 @@ const NavigationBar = () =>{
     return(
         <Navbar collapseOnSelect expand="md" className="NavBar navbar-static-top">
           <Navbar.Brand as={Link} to="/" className=' nav-name ms-2'>
-            {/* <img src="https://i.imgur.com/QLkSBI3.jpeg" alt='djmwLogo' style={{height: '3rem'}}/> */}
             Mike White
           </Navbar.Brand>
           <Nav className="ms-auto">
@@ -22,10 +21,10 @@ const NavigationBar = () =>{
                 <Nav.Link className='NavBar-link' as={Link} to="https://www.instagram.com/djmikewhite_/"><FontAwesomeIcon icon={faInstagram} size="2x" /></Nav.Link>
                 <Nav.Link className='NavBar-link' as={Link} to="https://soundcloud.com/user-554826787"><FontAwesomeIcon icon={faSoundcloud} size="2x" /></Nav.Link>
                 <Nav.Link className='NavBar-link' as={Link} to="https://www.facebook.com/mike.white.7739814"><FontAwesomeIcon icon={faSquareFacebook} size="2x" /></Nav.Link><Nav.Link className='NavBar-link' as={Link} to="https://x.com/?lang=en"><FontAwesomeIcon icon={faTwitter} size="2x" /></Nav.Link>
-                <Nav.Link className='NavBar-link' as={Link} to="/dj">DJ</Nav.Link>
-                <Nav.Link className='NavBar-link' as={Link} to="/actor">Actor</Nav.Link>
-                <Nav.Link className='NavBar-link' as={Link} to="/model">Model</Nav.Link>
                 <Nav.Link className='NavBar-link' as={Link} to="/">About</Nav.Link>
+                <Nav.Link className='NavBar-link' as={Link} to="/dj">DJ</Nav.Link>
+                <Nav.Link className='NavBar-link' as={Link} to="/model">Model</Nav.Link>
+                <Nav.Link className='NavBar-link' as={Link} to="/actor">Actor</Nav.Link>
                 <Nav.Link className='NavBar-link' as={Link} to="/Residencies">Residencies</Nav.Link>
                 <Nav.Link className='NavBar-link' as={Link} to="/contact">Contact</Nav.Link>
             </Navbar.Collapse>
