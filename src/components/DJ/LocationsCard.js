7853914760d11bd7da7eb28861d404918657ec87
SamStratton2112 <109560024+SamStratton2112@users.noreply.github.com>
2024-10-23 09:25:12 -0700

    updates

--- src/components/DJ/LocationsCard.js
+++ src/components/DJ/LocationsCard.js
@@ -5,16 +5,15 @@ import List from '../List';
 const LocationCard = ({location}) => {
     const [hidden, setHidden] = useState(true);
 
-    let body1 = <img src={location.image}  style={{ width: '19rem', height: '19rem',borderRadius:'.5em' }} alt={location.city} />;
+    let body1 = <img src={location.image}  style={{ width: '20rem', height: '20rem',borderRadius:'.5em' }} alt={location.city} />;
     
     let list = Array.from(location.locations)
     let body2 = <List list={list} />
     return (
-        <Col>
-                <p>{location.city}</p>
+        <Col sm={12} md={2} lg={3}>
+                <p style={{backgroundColor: 'rgb(13, 12, 12, .8)',  borderRadius:'.2em', marginTop:'1em'}}>{location.city}</p>
             <Container 
-                style={{ width: '20rem', height: '20rem' }} 
-                className="venueCard" 
+                style={{ width: '20rem', height: '20rem' }}
                 onMouseEnter={() => setHidden(false)} 
                 onMouseLeave={() => setHidden(true)}
             >
