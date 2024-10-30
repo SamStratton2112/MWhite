167547283e5889cc607553f01283efb87b79a39b
SamStratton2112 <109560024+SamStratton2112@users.noreply.github.com>
2024-10-29 18:10:07 -0700

    Update Video.js

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,46 +1,13 @@
-import { useRef } from 'react';
-import YouTube from 'react-youtube';
+import { Container } from 'react-bootstrap';
 
 const Video = ({ id }) => {
-    const playerRef = useRef(null); // Ref to store the player instance
 
-    const opts = {
-        height: '390',
-        width: '640',
-        playerVars: {
-            autoplay: 0, // Don't autoplay the video
-            loop: 0,
-        },
-    };
-
-    // Event handler for when the video is ready
-    const handleReady = (event) => {
-        playerRef.current = event.target; // Store player instance in ref
-        console.log("Video is ready but will not autoplay."); // Log readiness
-    };
-
-    // Example function to play the video when needed
-    // const playVideo = () => {
-    //     if (playerRef.current) {
-    //         playerRef.current.playVideo(); // Call play only if playerRef is set
-    //     } else {
-    //         console.error("Player is not initialized yet.");
-    //     }
-    // };
-
-    // You can call playVideo whenever you need, for example, with a button
-    // In this case, we'll just log a message to demonstrate it's ready
     return (
-        <div>
-            <YouTube 
-                videoId={id} 
-                opts={opts} 
-                onReady={handleReady} 
-                onError={(e) => console.error("An error occurred:", e.data)}
-            />
-            {/* If you want to trigger playVideo from somewhere else, you can uncomment the button */}
-            {/* <button onClick={playVideo}>Play Video</button> */}
-        </div>
+        <Container style={{width:"50%"}}>
+            <div className="ratio ratio-16x9" >
+                <iframe src={`https://www.youtube.com/embed/${id}?rel=0`} title="YouTube video" allowFullScreen></iframe>
+            </div>
+        </Container>
     );
 };
 
