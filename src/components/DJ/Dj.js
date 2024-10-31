9f11957718676254418bfe1d7d8dc4c3ea3a4571
SamStratton2112 <109560024+SamStratton2112@users.noreply.github.com>
2024-10-30 21:40:48 -0700

    clean up

--- src/components/DJ/Dj.js
+++ src/components/DJ/Dj.js
@@ -3,13 +3,12 @@ import MusicContainer from "./MusicContainer";
 import PrevExperience from "./PrevExperience";
 import AppearencesContainer from "./AppearencesContainer";
 import DjCarousel from "./DjCarousel";
-import './Dj.css';
 import Footer from '../Footer'
 
 const Dj = () => {
     return (
         <>
-        <Container className="dj cardText">
+        <Container className="cardText">
             <br/>
             <Container style={{backgroundColor: 'rgb(13, 12, 12, .85)', width:'20em', borderRadius:'.2em'}}><h3 style={{padding:'1em', marginBottom:'1em'}} >Albums</h3></Container>
             <MusicContainer />
