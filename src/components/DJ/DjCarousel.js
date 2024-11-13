eadd428ddf7727e46fa5279b429c3abe8b3e4bf9
SamStratton2112 <109560024+SamStratton2112@users.noreply.github.com>
2024-11-12 18:14:28 -0800

    Refactor Dj video carousel

--- src/components/DJ/DjCarousel.js
+++ src/components/DJ/DjCarousel.js
@@ -3,17 +3,14 @@ import Video from '../Video';
 import mike from '../../data';
 
 const DjCarousel = () => {
+    const videos = [];
+    for(let vid in mike.dj.links.video){
+         videos.push(<Carousel.Item interval={5000}><Video id={mike.dj.links.video[vid]} /></Carousel.Item>)
+    }
     return (
         <Container style={{paddingBottom: '5em'}}>
             <Carousel className='align-items-center'>
-                <Carousel.Item interval={5000}><Video id={mike.dj.links.video.liveARoll} /></Carousel.Item>
-                <Carousel.Item interval={5000}><Video id={mike.dj.links.video.laborDay} /></Carousel.Item>
-                <Carousel.Item interval={5000}><Video id={mike.dj.links.video.SirensCrush} /></Carousel.Item>
-                <Carousel.Item interval={5000}><Video id={mike.dj.links.video.carnitas} /></Carousel.Item>
-                <Carousel.Item interval={5000}><Video id={mike.dj.links.video.spiritsFest} /></Carousel.Item>
-                <Carousel.Item interval={5000}><Video id={mike.dj.links.video.elsewhere} /></Carousel.Item>
-                <Carousel.Item interval={5000}><Video id={mike.dj.links.video.rumor} /></Carousel.Item>
-                <Carousel.Item interval={5000}><Video id={mike.dj.links.video.wind} /></Carousel.Item>
+                {videos}
             </Carousel>
         </Container>
     );
