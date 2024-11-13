a73ea01db16b063f1752d3fe65e69d6cfc415c63
SamStratton2112 <109560024+SamStratton2112@users.noreply.github.com>
2024-11-12 16:50:14 -0800

    Update Residencies.js

--- src/components/Residencies.js
+++ src/components/Residencies.js
@@ -3,15 +3,20 @@ import mike from '../data.js';
 import VenueCard from './VenueCard';
 import Footer from './Footer'
 
+/** Loop through each residency object and create a venue card for it which displays the venue image except on hover => button linking to the venue's website. */ 
+
 function Residencies() {
+  const places= [];
+  for(let place in mike.dj.residencies){
+        places.push(<Col><VenueCard venue={mike.dj.residencies[place]}/></Col>)
+  }
+
   return (
     <>
     <Container style={{marginBottom: '3em'}}>
       <h1 style={{marginTop: '.5em', marginBottom:'1em'}}>Current Residencies</h1>
       <Row md={12} lg={3} className="containerBckground">
-        <Col><VenueCard venue={mike.dj.residencies.stk} /></Col>
-        <Col><VenueCard venue={mike.dj.residencies.carnitas} /></Col>
-        <Col><VenueCard venue={mike.dj.residencies.houseOfBlues} /></Col>
+        {places}
       </Row>
     </Container>
     <Footer/>
